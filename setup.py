6f8cc8ea5d3611fc2ebf2e0794df8b21f5786484
Baiju Muthukadan <baiju.m.mail@gmail.com>
2010-03-03 12:17:46 +0000

    - remove unused import - find-links is no more required
    git-svn-id: svn+ssh://fridge/stuff/zope-mirror/zope.app.zptpage/trunk@109606 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ $Id$
 
 import os
 
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
